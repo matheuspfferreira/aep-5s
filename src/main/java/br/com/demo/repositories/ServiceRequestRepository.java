816f7ee23e20e0dce302f1af8f6da2c45a234b6c
Matheus Pintor Fernandes Ferreira <mathfer1000@gmail.com>
2026-03-19 20:18:13 -0300

    feat: adicionar método de remoção de solicitações de serviços

--- src/main/java/br/com/demo/repositories/ServiceRequestRepository.java
+++ src/main/java/br/com/demo/repositories/ServiceRequestRepository.java
@@ -2,14 +2,13 @@ package br.com.demo.repositories;
 
 import br.com.demo.models.ServiceRequestModel;
 import org.json.simple.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceRequestRepository {
     private final List<JSONObject> serviceRequests;
 
-    public ServiceRequestRepository(List<JSONObject> serviceRequests) {
+    public ServiceRequestRepository() {
         this.serviceRequests = new ArrayList<>();
     }
 
@@ -17,16 +16,42 @@ public class ServiceRequestRepository {
         return this.serviceRequests;
     }
 
-    public void addServiceRequest(ServiceRequestModel serviceRequest) {
-        JSONObject serviceRequests = new JSONObject();
+    public Boolean alreadyAdded(Integer id) {
+        for (JSONObject request : this.serviceRequests) {
+            if (request.get("id").equals(id)) {
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
+    }
 
-        serviceRequests.put("id", serviceRequest.getId());
-        serviceRequests.put("description", serviceRequest.getDescription());
-        serviceRequests.put("category", serviceRequest.getCategory());
-        serviceRequests.put("priority", serviceRequest.getPriority());
-        serviceRequests.put("location", serviceRequest.getLocation());
-        serviceRequests.put("history", serviceRequest.getHistory());
+    public void save(ServiceRequestModel serviceRequest) throws IllegalArgumentException {
+        if (serviceRequest.getId() == null) {
+            throw new IllegalArgumentException("A solicitação de serviço deve ter obrigatoriamente um ID");
+        }
 
-        this.serviceRequests.add(serviceRequests);
+        if (alreadyAdded(serviceRequest.getId())) {
+            throw new IllegalArgumentException(
+                    "Uma solicitação de serviço com esse ID já foi adicionada: " + serviceRequest.getId()
+            );
+        }
+
+        JSONObject newServiceRequest = new JSONObject();
+        newServiceRequest.put("id", serviceRequest.getId());
+        newServiceRequest.put("description", serviceRequest.getDescription());
+        newServiceRequest.put("category", serviceRequest.getCategory());
+        newServiceRequest.put("priority", serviceRequest.getPriority());
+        newServiceRequest.put("location", serviceRequest.getLocation());
+        newServiceRequest.put("history", serviceRequest.getHistory());
+
+        this.serviceRequests.add(newServiceRequest);
+    }
+
+    public void removeById(Integer id) {
+        this.serviceRequests.removeIf(request -> request.get("id").equals(id));
+    }
+
+    public List<JSONObject> getServiceRequests() {
+        return this.serviceRequests;
     }
 }
